16f32de869a1424eac4f143f3b77a4de3a11a14b
Ringo Hoffmann <rh@digitronic.net>
2020-08-17 16:21:07 +0200

    add idgenerator module

---
+++ modules/idgenerator.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers#417184
+const MAX_SIZE = 2000 - 100; // Max theoretical URL size minus domain name, sub domain, TLD and bla.
+
+function getOneHundredPercentUniqueID() {}
+
+module.exports = getOneHundredPercentUniqueID;
