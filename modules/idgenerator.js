b8a764900431a82db0a74728ab0345735381f73d
zekroTJA <riho@live.de>
2020-08-18 12:13:15 +0200

    ensure links always ends with a character

--- modules/idgenerator.js
+++ modules/idgenerator.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 // https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers#417184
 const MAX_SIZE = 2000 - 100; // Max theoretical URL size minus domain name, sub domain, TLD and bla.
-const CHARS =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/[]@!$&'()*+,;=";
+const CHARS_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const CHARS = CHARS_ALPHABET + "0123456789-._~:/[]@!$&'()*+,;=";
 
 // Function used to generate character repetations to make
 // the final URL even more uglier.
@@ -15,17 +15,23 @@ const multiplierFunc = (v) => Math.pow(v, 20) * 10 + 1;
  * to make sure. Not just to make the link longer.
  */
 function getOneHundredPercentUniqueID() {
-  let str = "";
+  let str = '';
   do {
     str += getRandChars();
   } while (str.length <= MAX_SIZE);
+  // Ensure that link always ends with a character to dodge
+  // cutting off the link in messangers.
+  return str.substr(0, MAX_SIZE) + getRandChar(CHARS_ALPHABET);
+}
 
-  return str.substr(0, MAX_SIZE);
+function getRandChar(chars) {
+  const cset = chars || CHARS;
+  const i = Math.floor(random(0, cset.length));
+  return cset[i];
 }
 
 function getRandChars() {
-  const i = Math.floor(random(0, CHARS.length));
-  const char = CHARS[i];
+  const char = getRandChar();
 
   const mult = Math.floor(multiplierFunc(random(0, 1)));
   return char.repeat(mult);
